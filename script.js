a7cc7d76900e91c4cc061622601014deafba8f20
HaroshiyGrigoriy <guruwolf8@yandex.ru>
2025-04-03 20:18:11 +0300

    update index and script.js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 function checkPassword() {
-  const input = document.getElementById("password").value;
-  const correctPassword = "grisha123";
-
-  if (input === correctPassword) {
-    // В будущем здесь будет запрос к Spring Boot
-    window.location.href = "nextstep.html"; // или вызов API
-  } else {
-    document.getElementById("error").textContent = "Неверный пароль!";
-  }
+  document.getElementById("loginBtn").addEventListener("click", async () => {
+    const password = document.getElementById("passwordInput").value;
+    try {
+      const response = await fetch("http://localhost:8080/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ password: password })
+      });
+      if (response.ok) {
+        window.location.href = "nextstep.html";
+      } else {
+        alert("Вы ввели неверный пароль");
+      }
+    } catch (error) {
+      console.error("Произошла ошибка запроса:", error);
+    }
+  });
 }
+
