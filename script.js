59b5ae03fc1f22e33b25941136c327fa814abee5
guruw <guruwolf8@gmail.com>
2025-04-04 11:09:05 +0300

    update style for p,h1,button. create sidebar,and function js for sidebar

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 function checkPassword() {
-  const input = document.getElementById("password").value;
-  const correctPassword = "grisha123";
-
-  if (input === correctPassword) {
-    // В будущем здесь будет запрос к Spring Boot
-    window.location.href = "nextstep.html"; // или вызов API
-  } else {
-    document.getElementById("error").textContent = "Неверный пароль!";
-  }
+  document.getElementById("loginBtn").addEventListener("click", async () => {
+    const password = document.getElementById("passwordInput").value;
+    try {
+      const response = await fetch("http://localhost:8080/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ password: password })
+      });
+      if (response.ok) {
+        window.location.href = "nextstep.html";
+      } else {
+        alert("Вы ввели неверный пароль");
+      }
+    } catch (error) {
+      console.error("Произошла ошибка запроса:", error);
+    }
+  });
 }
+
 function redirectToPage(event){
   event.preventDefault();
   var selectedUrl = document.getElementById("position").value;
@@ -23,3 +32,17 @@ document.addEventListener("DOMContentLoaded", function() {
   var form=document.getElementById("positionForm");
   form.addEventListener('submit',redirectToPage);
 })
+
+function initSideBarToggle(){
+  const navToggel=document.querySelector('.nav-toggle');
+  const sideBar=document.getElementById('sidebar');
+if(!navToggel || !sideBar) {
+  console.warn('navToggle или sideBar не найдены' );
+  return;
+}
+navToggel.addEventListener('click',
+  () => { sideBar.classList.toggle('sidebar--open');
+  });
+}
+  document.addEventListener('DOMContentLoaded',
+    () => { initSideBarToggle();});
